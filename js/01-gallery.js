61da77849bb6a3c0065c0e877f306c5fc7f2b9f1
Tetiana <tania.pokhylko@gmail.com>
2023-05-16 22:23:03 +0300

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markUp = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+          <img
+            class='gallery__image'
+            src='${preview}'
+            data-source='${original}'
+            alt='${description}'
+          />
+        </a>
+      </li>`
+);
+
+galleryEl.insertAdjacentHTML('afterbegin', markUp.join(''));
+
+galleryEl.addEventListener('click', onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const imgDataSource = event.target.dataset.source;
+
+  const currentImg = event.target;
+
+  const instance = basicLightbox.create(`
+  <div class="modal">
+            <img
+            class='gallery__image'
+            src= ${imgDataSource}
+            
+            alt='${currentImg.description}'
+          />
+        </div>
+     `);
+
+  instance.show();
+}
